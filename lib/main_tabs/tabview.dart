0efc34c4c8eb7451d4323832f027065aeabc34d3
HayHay <dev@hayhay.dev>
2024-04-01 23:47:55 -0400

    Redone the bottom app bar code

--- lib/main_tabs/tabview.dart
+++ lib/main_tabs/tabview.dart
@@ -2,10 +2,8 @@ import 'package:better_assignments/main_tabs/completed.dart';
 import 'package:better_assignments/alt_screens/settings/settings.dart';
 import 'package:better_assignments/main_tabs/star.dart';
 import 'package:better_assignments/main_tabs/home.dart';
-import 'package:flutter/cupertino.dart';
+import 'package:better_assignments/slidable_widgets/assign_widgets.dart';
 import 'package:flutter/material.dart';
-import 'package:persistent_bottom_nav_bar/persistent-tab-view.dart';
-import '../slidable_widgets/assign_widgets.dart';
 
 class TabView extends StatefulWidget {
   @override
@@ -13,102 +11,45 @@ class TabView extends StatefulWidget {
 }
 
 class _TabViewState extends State<TabView> {
-  PersistentTabController _controller =
-      PersistentTabController(initialIndex: 0);
+  int _selectedIndex = 0;
+
+  List<Widget> _pages = [
+    Home(),
+    Star(),
+    Home(),
+    Completed(),
+    Settings(),
+  ];
+
+  void _onItemTapped(int index) async {
+    if (index == 2) {
+      // Call the assignModal function when the add button is tapped
+      await assignModal(true, null, "");
+    } else {
+      // Update the selected index for other buttons
+      setState(() {
+        _selectedIndex = index;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return _tabView();
-  }
-
-  Widget _tabView() {
-    return PersistentTabView(
-      context,
-      controller: _controller,
-      navBarHeight: 60,
-      screens: _buildScreens(),
-      items: _navBarsItems(),
-      confineInSafeArea: true,
-      backgroundColor: Colors.black,
-      handleAndroidBackButtonPress: true,
-      resizeToAvoidBottomInset: true,
-      stateManagement: true,
-      hideNavigationBarWhenKeyboardShows: true,
-      decoration: NavBarDecoration(
-        borderRadius: BorderRadius.circular(30.0),
-        colorBehindNavBar: Colors.transparent,
-      ),
-      popAllScreensOnTapOfSelectedTab: true,
-      popActionScreens: PopActionScreensType.all,
-      itemAnimationProperties: ItemAnimationProperties(
-        // Navigation Bar's items animation properties.
-        duration: Duration(milliseconds: 200),
-        curve: Curves.ease,
-      ),
-      screenTransitionAnimation: ScreenTransitionAnimation(
-        // Screen transition animation on change of selected tab.
-        animateTabTransition: true,
-        curve: Curves.ease,
-        duration: Duration(milliseconds: 200),
-      ),
-      navBarStyle:
-          NavBarStyle.style7, // Choose the nav bar style with this property.
-      onItemSelected: (index) {
-        setState(() {});
-      },
-    );
-  }
-
-  List<Widget> _buildScreens() {
-    return [
-      Home(),
-      Star(),
-      Home(),
-      Completed(),
-      Settings(),
-    ];
-  }
-
-  List<PersistentBottomNavBarItem> _navBarsItems() {
-    return [
-      PersistentBottomNavBarItem(
-        icon: Icon(Icons.home),
-        title: "Home",
-        textStyle: TextStyle(color: Colors.white),
-        activeColorSecondary: Colors.white,
-        activeColorPrimary: Colors.purple,
-      ),
-      PersistentBottomNavBarItem(
-        icon: Icon(Icons.star),
-        title: ("Prioritized"),
-        textStyle: TextStyle(color: Colors.white),
-        activeColorSecondary: Colors.white,
-        activeColorPrimary: Colors.amber,
-      ),
-      PersistentBottomNavBarItem(
-        icon: Icon(Icons.add),
-        title: ("Add"),
-        textStyle: TextStyle(color: Colors.white),
-        activeColorSecondary: Colors.white,
-        activeColorPrimary: Colors.white,
-        onPressed: (context) async {
-          await assignModal(true, null, "");
-          setState(() {});
-        },
-      ),
-      PersistentBottomNavBarItem(
-        icon: Icon(Icons.check_box),
-        title: ("Completed"),
-        activeColorPrimary: Colors.green,
-        textStyle: TextStyle(color: Colors.white),
-        activeColorSecondary: Colors.white,
-      ),
-      PersistentBottomNavBarItem(
-        icon: Icon(Icons.settings),
-        title: ("Settings"),
-        activeColorPrimary: Colors.blue,
-        textStyle: TextStyle(color: Colors.blue),
-        activeColorSecondary: Colors.white,
-      ),
-    ];
+    return Scaffold(
+        body: _pages[_selectedIndex],
+        bottomNavigationBar: NavigationBar(
+            onDestinationSelected: _onItemTapped,
+            selectedIndex: _selectedIndex,
+            indicatorColor: Colors.purple,
+            destinations: [
+              NavigationDestination(icon: Icon(Icons.home), label: 'Home'),
+              NavigationDestination(
+                  icon: Icon(Icons.star), label: "Prioritized"),
+              NavigationDestination(icon: Icon(Icons.add), label: "Add"),
+              NavigationDestination(
+                  icon: Icon(Icons.check_box), label: "Completed"),
+              NavigationDestination(
+                  icon: Icon(Icons.settings), label: "Settings"),
+            ]));
   }
 }
